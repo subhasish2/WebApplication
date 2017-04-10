c5274f75f001fa3e52244acde2132089184f355d
subhasish2 <subhasish.ghosh1992@gmail.com>
2017-04-11 00:17:21 +0530

    getCentroid method implemented in Cluster class.

--- Login/src/com/Clustering/Cluster.java
+++ Login/src/com/Clustering/Cluster.java
@@ -37,5 +37,36 @@ public class Cluster {
 			System.out.println(l);
 		}
 	}
+	public Location getCentroid(){
+		double x=0,y=0,z=0;
+		for(Location l:points) {
+			double lat = Math.toRadians(l.getLatitude());
+			double lon = Math.toRadians(l.getLongitude());
+			 x += Math.cos(lat) * Math.cos(lon);
+	         y += Math.cos(lat) * Math.sin(lon);
+	         z += Math.sin(lat);
+		}
+		int count=points.size();
+		x/=count;
+		y/=count;
+		z/=count;
+		Double centralLongitude = Math.atan2(y, x);
+        Double centralSquareRoot = Math.sqrt(x * x + y * y);
+        Double centralLatitude = Math.atan2(z, centralSquareRoot);
+        Double latitude=Math.toDegrees(centralLatitude);
+        Double longitude=Math.toDegrees(centralLongitude);
+        Location center=new Location("", latitude, longitude);
+        double min_distance=center.distanceTo(points.get(0));
+        Location centroid=points.get(0);
+        for(int i=1;i<points.size();i++) {
+        	Location l=points.get(i);
+        	Double distance=center.distanceTo(l);
+        	if(distance<min_distance) {
+        		min_distance=distance;
+        		centroid=l;
+        	}
+        }
+        return centroid;
+	}
 	
 }
